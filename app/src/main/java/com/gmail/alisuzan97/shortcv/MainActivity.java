4cfd62b80f0d633b42ad9c259ab74daf9db8a1af
SuzanneAli <alisuzanne97@gmail.com>
2021-04-27 23:19:30 +0300

    first version

---
+++ app/src/main/java/com/gmail/alisuzan97/shortcv/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.alisuzan97.shortcv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
